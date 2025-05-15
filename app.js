70679ce0cfa7dd5409aa8680b0f981e2ad899e74
Kirill <VolandB@gmail.com>
2025-05-15 22:22:46 +0300

    initial commit with express

--- app.js
+++ app.js
@@ -159,5 +159,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+    const PORT = process.env.PORT || 3000;
+    console.log(`Server started on http://localhost:${PORT}`);
 }); 
